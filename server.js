d395b76ee6a8bbb8c6db6464e49f04aaddc7cabe
rafael.evaristo <rafael.evaristo@gmail.com>
2025-03-15 16:45:29 +0000

    Changed to privide a simple static server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use strict';
 
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-	res.send('De dentro da cena !\n');
+const app = new express();
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, HOST);
